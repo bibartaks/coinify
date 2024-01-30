1bbf5828675f55f250d4cf03a71fd697b13bb9fd
bibartaks <bibartaks@gmail.com>
2024-01-30 22:48:58 +0600

    fix: Wrap page.tsx with Suspense to resolve deployment issues

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,18 @@
-import Landing from "@/components/Landing/Landing"
-import Trending from "@/components/Trending/Trending"
-import MarketCoins from "@/components/MarketCoins/MarketCoins"
-import GlobalInfo from "@/components/GlobalInfo/GlobalInfo"
+import GlobalInfo from "@/components/GlobalInfo/GlobalInfo";
+import Landing from "@/components/Landing/Landing";
+import MarketCoins from "@/components/MarketCoins/MarketCoins";
+import Trending from "@/components/Trending/Trending";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
-    <div className="px-5">
-      <Landing />
-      <GlobalInfo />
-      <Trending />
-      <MarketCoins />
-    </div>
-  )
+    <Suspense>
+      <div className="px-5">
+        <Landing />
+        <GlobalInfo />
+        <Trending />
+        <MarketCoins />
+      </div>
+    </Suspense>
+  );
 }
